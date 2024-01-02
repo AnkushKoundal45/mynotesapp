88e7263e30afec2a00113f65ad718e2b00f45420
AnkushKoundal45 <22bme020@nith.ac.in>
2024-01-02 20:37:08 +0530

    step24

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
 import 'package:mynotes/utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -57,35 +57,32 @@ class _LoginViewState extends State<LoginView> {
             autocorrect: false,
             controller: _password,
           ),
-          TextButton(
-              onPressed: () async {
-                try {
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) async {
+              if (state is AuthStateLoggedOut) {
+                if (state.exception is InvalidLoginCredentials) {
+                  return await showErrorDialog(
+                      context, 'Invalid E-mail or Password');
+                } else if (state.exception is ChannelErrorAuthException) {
+                  return await showErrorDialog(
+                      context, 'Enter your E-mail or Password');
+                } else if (state.exception is InvalidEmailAuthException) {
+                  return await showErrorDialog(context, 'Invalid E-mail');
+                } else if (state.exception is GenericAuthException) {
+                  return await showErrorDialog(context, 'Authentication Error');
+                }
+              }
+            },
+            child: TextButton(
+                onPressed: () async {
                   final email = _email.text;
                   final password = _password.text;
                   context.read<AuthBloc>().add(
                         AuthEventLogIn(email, password),
                       );
-                } on InvalidLoginCredentials {
-                  return await showErrorDialog(
-                      context, 'Invalid E-mail or Password');
-                } on ChannelErrorAuthException {
-                  await showErrorDialog(
-                    context,
-                    'Enter your E-mail or Password',
-                  );
-                } on InvalidEmailAuthException {
-                  await showErrorDialog(
-                    context,
-                    'Invalid E-mail',
-                  );
-                } on GenericAuthException {
-                  await showErrorDialog(
-                    context,
-                    'Authentication Error',
-                  );
-                }
-              },
-              child: const Text('Login')),
+                },
+                child: const Text('Login')),
+          ),
           TextButton(
               onPressed: () {
                 Navigator.of(context)
